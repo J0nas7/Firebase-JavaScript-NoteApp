6c8f8396677781ca4152ff85b5ed634c31691cea
Jonas Sørensen <jonas7793@gmail.com>
2020-10-01 22:29:15 +0200

    More responsive design with an awesome menu trigger

--- main.js
+++ main.js
@@ -22,4 +22,17 @@ var datestamp = new Date();
 document.getElementById("theNoteDate").innerHTML =  datestamp.getDate()+". "+
                                                     datestamp.toLocaleString('default', { month: 'long' })+" "+
                                                     datestamp.getFullYear()+" - "+
-                                                    datestamp.getHours()+":"+(datestamp.getMinutes() < 10 ? "0" : "")+datestamp.getMinutes()+":"+(datestamp.getSeconds() < 10 ? "0" : "")+datestamp.getSeconds();
+                                                    (datestamp.getHours() < 10 ? "0" : "")+datestamp.getHours()+":"+(datestamp.getMinutes() < 10 ? "0" : "")+datestamp.getMinutes()+":"+(datestamp.getSeconds() < 10 ? "0" : "")+datestamp.getSeconds();
+
+
+
+/*document.getElementById("noteListTrigger").addEventListener("click", () => {
+    
+});*/
+
+let menuTrigger = document.getElementById("menuTrigger");
+let noteList = document.getElementById("noteList");
+menuTrigger.addEventListener("click", () => {
+    menuTrigger.classList.toggle("triggered");
+    noteList.classList.toggle("open");
+});
