eb6c8da219be158c58374be7ec66a0ec9ab64142
Jonas Sørensen <jonas7793@gmail.com>
2020-10-07 10:22:29 +0200

    Pretty much finished

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-/*var datestamp = new Date();
-document.getElementById("theNoteDate").innerHTML =  datestamp.getDate()+". "+
-                                                    datestamp.toLocaleString('default', { month: 'long' })+" "+
-                                                    datestamp.getFullYear()+" - "+
-                                                    (datestamp.getHours() < 10 ? "0" : "")+datestamp.getHours()+":"+(datestamp.getMinutes() < 10 ? "0" : "")+datestamp.getMinutes()+":"+(datestamp.getSeconds() < 10 ? "0" : "")+datestamp.getSeconds();*/
-
-
-
 /*document.getElementById("noteListTrigger").addEventListener("click", () => {
     
 });*/
@@ -17,6 +9,21 @@ menuTrigger.addEventListener("click", () => {
     noteList.classList.toggle("open");
 });
 
+function timestampToReadableDate(timestamp) {
+    var datestamp = new Date();
+    datestamp.setTime(timestamp);
+    var returnDate =    datestamp.getDate()+". "+
+                        datestamp.toLocaleString('default', { month: 'long' })+" "+
+                        datestamp.getFullYear()+" - "+
+                        (datestamp.getHours() < 10 ? "0" : "")+datestamp.getHours()+":"+(datestamp.getMinutes() < 10 ? "0" : "")+datestamp.getMinutes()+":"+(datestamp.getSeconds() < 10 ? "0" : "")+datestamp.getSeconds();
+    return returnDate;
+}
 
-
-
+function timestampToShortDate(timestamp) {
+    var datestamp = new Date();
+    datestamp.setTime(timestamp);
+    var returnDate =    (datestamp.getDate() < 10 ? "0" : "")+datestamp.getDate()+"."+
+                        (datestamp.getMonth() < 10 ? "0" : "")+datestamp.getMonth()+"."+
+                        datestamp.getFullYear().toString().slice(-2);
+    return returnDate;
+}
--- notecrud.js
+++ notecrud.js
@@ -1,35 +1,198 @@
-/*db.collection("notes").add({
-    title: "Hello 321",
-    date: new Date().getTime(),
-    content: "Lorem ipsum dolor sit amet"
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});*/
-
+class Note {
+    constructor(title, date, content, id, star) {
+        this.title = title;
+        this.date = date;
+        this.content = content;
+        this.id = id;
+        this.star = star;
+    }
+}
+var noteArray = [];
+var showFavs = false;
 let theNotes = document.getElementsByClassName("notes")[0];
+document.getElementById("theNoteTitle").focus();
 
-var selectedIndex = 1;
-var i = 1;
-db.collection("notes").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id}`);
-        console.log(doc.data());
-        theNotes.innerHTML +=   "<div class='aNote' id='n"+i+"'>"+
-                                "<span class='nTitle'>"+doc.data().title+"</span>"+
-                                "<span class='nDate'>today</span>"+
-                                "<span class='nContent'>"+doc.data().content+"...</span>"+
+function createNote() {
+    var now = new Date().getTime();
+    db.collection("notes").add({
+        title: document.getElementById("theNoteTitle").value,
+        date: now,
+        content: document.getElementById("theNoteContent").value,
+        star: "no"
+    })
+    .then(function(docRef) {
+        document.getElementById("theNoteId").value = docRef.id;
+        noteArray.push(new Note(
+                                    document.getElementById("theNoteTitle").value, 
+                                    now, 
+                                    document.getElementById("theNoteContent").value, 
+                                    docRef.id,
+                                    "no"
+                                ));
+        updateNoteList();
+        readNote(docRef.id);
+    });
+}
+
+function updateNote(star = "no") {
+    var now = new Date().getTime();
+    var theId = document.getElementById("theNoteId").value;
+    db.collection("notes").doc(theId).update({
+        title: document.getElementById("theNoteTitle").value,
+        date: now,
+        content: document.getElementById("theNoteContent").value,
+        star: star
+    }).then(()=>{
+        var theNote = noteArray.filter((note) => (note.id == theId))[0];
+        theNote.title = document.getElementById("theNoteTitle").value;
+        theNote.date =  now;
+        theNote.content = document.getElementById("theNoteContent").value;
+        theNote.star = star;
+        updateNoteList();
+        readNote(theId);
+    });
+}
+
+function updateNoteList() {
+    theNotes.innerHTML = "";
+    noteArray.sort((a, b) => (b.date > a.date) ? 1 : -1);
+    var theArray = noteArray;
+    if (showFavs) {
+        theArray = noteArray.filter((note)=>(note.star == "yes"));
+    }
+    document.getElementById("noteListCount").innerHTML = "("+theArray.length+")";
+    theArray.forEach((note) => {
+        theNotes.innerHTML +=   "<div class='aNote' id='"+note.id+"'>"+
+                                "<span class='nTitle'>"+note.title+"</span>"+
+                                "<span class='nDate'>"+timestampToShortDate(note.date)+"</span>"+
+                                "<span class='nContent'>"+note.content.slice(0,10)+"...</span>"+
                                 "<div class='clrBth fltLft'></div>"+
                                 "</div>";
-        
-        if (i == selectedIndex) {
-            document.getElementById("theNoteTitle").innerHTML = doc.data().title;
-            document.getElementById("theNoteContent").innerHTML = doc.data().content;
-        }
-        i++;
     });
-    document.getElementById("n"+selectedIndex).classList.add("active");
+    notesEventListener();
+}
+
+function readNote(noteId) {
+    var theNote = noteArray.filter((note) => (note.id == noteId))[0];
+    console.log(theNote);
+    document.getElementById("theNoteId").value = theNote.id;
+    document.getElementById("theNoteStar").value = theNote.star;
+    document.getElementById("theNoteTitle").value = theNote.title;
+    document.getElementById("theNoteDate").innerHTML = timestampToReadableDate(theNote.date);
+    document.getElementById("theNoteContent").value = theNote.content;
+    document.getElementById("notemenu").style.display = "block";
+
+    document.getElementById("marked").style.display = (theNote.star == "yes" ? "block" : "none");
+    document.getElementById("unmarked").style.display = (theNote.star == "no" || theNote.star == undefined ? "block" : "none");
+
+    menuTrigger.classList.remove("triggered");
+    noteList.classList.remove("open");
+    
+    var items = document.getElementsByClassName("aNote");
+    for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove("active");
+    }
+    document.getElementById(theNote.id).classList.add("active");
+}
+
+function loadAllNotes() {
+    db.collection("notes").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            noteArray.push(new Note(doc.data().title, doc.data().date, doc.data().content, doc.id, doc.data().star));
+        });
+        updateNoteList();
+    });
+}
+loadAllNotes();
+
+function notesEventListener() {
+    var elements = document.getElementsByClassName("aNote");
+    var findNote = function() {
+        var noteId = this.getAttribute("id");
+        readNote(noteId);
+    };
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', findNote, false);
+    }
+}
+
+document.getElementById("newNote").addEventListener("click", () => {
+    document.getElementById("theNoteId").value = "";
+    document.getElementById("theNoteTitle").value = "";
+    document.getElementById("theNoteTitle").focus();
+    document.getElementById("theNoteDate").innerHTML = "today";
+    document.getElementById("theNoteContent").value = "";
+    document.getElementById("notemenu").style.display = "none";
+    updateNoteList();
 });
+
+document.getElementById("deleteNote").addEventListener("click", () => {
+    var noteId = document.getElementById("theNoteId").value;
+    if (noteId) {
+        db.collection('notes').doc(noteId).delete();
+        noteArray = noteArray.filter((note) => (note.id != noteId));
+        
+        document.getElementById("theNoteId").value = "";
+        document.getElementById("theNoteTitle").value = "";
+        document.getElementById("theNoteTitle").focus();
+        document.getElementById("theNoteDate").innerHTML = "today";
+        document.getElementById("theNoteContent").value = "";
+        document.getElementById("notemenu").style.display = "none";
+        updateNoteList();
+    } else {
+        alert("Select a note you want to delete!");
+    }
+});
+
+document.getElementById("toggleImportant").addEventListener("click", () => {
+    var noteId = document.getElementById("theNoteId").value;
+    var noteStar = document.getElementById("theNoteStar").value;
+    if (noteId) {
+        if (noteStar == "no" || noteStar == "undefined") {
+            updateNote("yes");
+        } else if (noteStar == "yes") {
+            updateNote();
+        }
+    } else {
+        alert("Select a note you want to mark as important!");
+    }
+});
+
+document.getElementById("noteFavs").addEventListener("click", () => {
+    if (!showFavs) {
+        document.getElementById("noteFavs").innerHTML = "&#9733;";
+        showFavs = true;
+    } else if (showFavs) {
+        document.getElementById("noteFavs").innerHTML = "&#9734;";
+        showFavs = false;
+    }
+    updateNoteList();
+});
+
+// typing listener
+let typingTimer;                //timer identifier
+let doneTypingInterval = 3500;  //time in ms (5 seconds)
+let listenTitle = document.getElementById('theNoteTitle');
+let listenContent = document.getElementById('theNoteContent');
+
+//on keyup, start the countdown
+listenTitle.addEventListener('keyup', () => { elementCheck(); });
+listenContent.addEventListener('keyup', () => { elementCheck(); });
+
+function elementCheck() {
+    clearTimeout(typingTimer);
+    if (listenTitle.value && listenContent.value) {
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    }
+}
+
+//user is "finished typing," do something
+function doneTyping () {
+    if (document.getElementById("theNoteId").value) {
+        // update an existing note
+        updateNote();
+    } else {
+        // create a new note
+        createNote();
+    }
+}
